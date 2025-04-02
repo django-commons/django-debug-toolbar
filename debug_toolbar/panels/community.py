9efda5803b0226efab27f0f8c6ab06fbb0656d3a
andoriyaprashant <prashantandoriya@gmail.com>
2025-04-02 23:49:02 +0530

    Added Community panel

---
+++ debug_toolbar/panels/community.py
@@ -0,0 +1,13 @@
+﻿from debug_toolbar.panels import Panel
+from django.utils.translation import gettext_lazy as _
+
+class CommunityPanel(Panel):
+    """
+    A panel that provides links to the Django Debug Toolbar community.
+    """
+
+    title = _("Community")
+    template = "debug_toolbar/panels/community.html"
+
+    def nav_title(self):
+        return _("Community")
