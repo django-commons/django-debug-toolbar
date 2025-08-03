3172bbbd0e63274755f8303569b9a6c9088a8597
rbanik30082002 <banikrangonath2002@gmail.com>
2025-08-03 13:03:52 +0530

    Added the below code in line 9 from selenium.webdriver.common.action_chains import ActionChains Added # type: ignore coomad to avoid the eeror in selenium

--- example/screenshot.py
+++ example/screenshot.py
@@ -6,6 +6,7 @@ import subprocess
 from time import sleep
 
 from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.common.action_chains import ActionChains # type: ignore
 from selenium.webdriver.common.keys import Keys # type: ignore
 from selenium.webdriver.support import expected_conditions as EC # type: ignore
 from selenium.webdriver.support.wait import WebDriverWait # type: ignore
