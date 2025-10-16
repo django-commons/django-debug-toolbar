07b057be5547ee1bc41242fa47eb6a7b028f9f24
James Gutu <jmgutu@innovexsolutions.co.ke>
2025-10-17 00:34:55 +0300

    added tests

--- debug_toolbar/panels/sql/tracking.py
+++ debug_toolbar/panels/sql/tracking.py
@@ -128,10 +128,8 @@ class NormalCursorMixin(DjDTCursorWrapperMixin):
 
         # make sure datetime, date and time are converted to string by force_str
         CONVERT_TYPES = (datetime.datetime, datetime.date, datetime.time)
-        try:
-            return force_str(param, strings_only=not isinstance(param, CONVERT_TYPES))
-        except UnicodeDecodeError:
-            return "(encoded string)"
+        return force_str(param, strings_only=not isinstance(param, CONVERT_TYPES))
+
 
     def _last_executed_query(self, sql, params):
         """Get the last executed query from the connection."""
--- debug_toolbar/sanitize.py
+++ debug_toolbar/sanitize.py
@@ -1,8 +1,14 @@
 from django.utils.encoding import DjangoUnicodeDecodeError, force_str as force_string
 
 
-def force_str(value):
+# def force_str(s, encoding="utf-8", strings_only=False, errors="strict"):
+#     try:
+#         return force_string(s, encoding, strings_only, errors)
+#     except DjangoUnicodeDecodeError:
+#         return "Django Debug Toolbar was unable to parse value."
+
+def force_str(s, *args, **kwargs):
     try:
-        return force_string(value)
+        return force_string(s, *args, **kwargs)
     except DjangoUnicodeDecodeError:
         return "Django Debug Toolbar was unable to parse value."
