517ae7da3875cae8ed1478f13dcc3c53333639f4
James Gutu <jmgutu@innovexsolutions.co.ke>
2025-10-17 00:38:29 +0300

    added comment to sanitize.py

--- debug_toolbar/sanitize.py
+++ debug_toolbar/sanitize.py
@@ -1,13 +1,11 @@
 from django.utils.encoding import DjangoUnicodeDecodeError, force_str as force_string
 
 
-# def force_str(s, encoding="utf-8", strings_only=False, errors="strict"):
-#     try:
-#         return force_string(s, encoding, strings_only, errors)
-#     except DjangoUnicodeDecodeError:
-#         return "Django Debug Toolbar was unable to parse value."
-
 def force_str(s, *args, **kwargs):
+    '''
+    Forces values to strings.
+    Will return "Django Debug Toolbar was unable to parse value." when there's a decoding error.
+    '''
     try:
         return force_string(s, *args, **kwargs)
     except DjangoUnicodeDecodeError:
