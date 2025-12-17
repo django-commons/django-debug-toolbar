fe4e3051d353c3fe7680030b2868507c0a3b9cea
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-12-17 19:47:24 +0000

    Refactor profiling panel and add download functionality for .prof files

--- debug_toolbar/urls.py
+++ debug_toolbar/urls.py
@@ -1,14 +1,6 @@
-from django.urls import path
-
-from debug_toolbar import APP_NAME, views as debug_toolbar_views
+from debug_toolbar import APP_NAME
 from debug_toolbar.toolbar import DebugToolbar
 
 app_name = APP_NAME
 
-urlpatterns = DebugToolbar.get_urls() + [
-    path(
-        "download_prof_file/",
-        debug_toolbar_views.download_prof_file,
-        name="debug_toolbar_download_prof_file",
-    ),
-]
+urlpatterns = DebugToolbar.get_urls()
